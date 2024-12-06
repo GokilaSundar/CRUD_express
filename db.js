a18a6cf88422fe184b050333b8fe9a6cf6bfe9e3
GokilaSundar <gokithilaga267@gmail.com>
2024-12-06 16:10:19 +0530

    Setup mongoose in db file

--- db.js
+++ db.js
@@ -1,27 +1,47 @@
-const mongodb = require("mongodb");
-const mongoClient = mongodb.MongoClient; //usedto connect with db
+// const mongodb = require("mongodb");
+// const mongoClient = mongodb.MongoClient; //usedto connect with db
 
-const objectId = mongodb.ObjectId; //to get exact id from mongo we used object Id from mongodb
+// const objectId = mongodb.ObjectId; //to get exact id from mongo we used object Id from mongodb
 
-const uri = "mongodb://localhost:27017";
-const client = new mongoClient(uri);
-var database;
+// const uri = "mongodb://localhost:27017";
+// const client = new mongoClient(uri);
+// var database;
 
+// const getDatabase = async () => {
+//   try {
+//     if (!database) {
+//       await client.connect();
+//       database = client.db("library");
+//     }
+//     return database;
+//   } catch (err) {
+//     console.log("Problem to create an Database");
+//   } finally {
+//     console.log("Database successfully created");
+//   }
+// };
+
+// module.exports = {
+//   getDatabase,
+//   objectId,
+// };
+
+//
+
+//
+
+///------///----///
+
+const mongoose = require("mongoose");
 const getDatabase = async () => {
-  try {
-    if (!database) {
-      await client.connect();
-      database = client.db("library");
-    }
-    return database;
-  } catch (err) {
-    console.log("Problem to create an Database");
-  } finally {
-    console.log("Database successfully created");
-  }
+  mongoose
+    .connect("mongodb://localhost:27017/library")
+    .then(() => console.log("Database connected successfully using mongoose"))
+    .catch(() =>
+      console.log("Error while connected with database using mongoose")
+    );
 };
 
 module.exports = {
   getDatabase,
-  objectId,
 };
